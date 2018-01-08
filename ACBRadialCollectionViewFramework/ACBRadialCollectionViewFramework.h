76da36937593a563b7391b63ae09b8e0b95b78fe
Akhil <akhilcb@yahoo.com>
2018-01-08 00:09:46 -0800

    Added new framework target

---
+++ ACBRadialCollectionViewFramework/ACBRadialCollectionViewFramework.h
@@ -0,0 +1,17 @@
+//
+//  ACBRadialCollectionViewFramework.h
+//  ACBRadialCollectionViewFramework
+//
+//  Created by Akhil C Balan on 1/8/18.
+//  Copyright © 2018 akhil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACBRadialCollectionViewFramework.
+FOUNDATION_EXPORT double ACBRadialCollectionViewFrameworkVersionNumber;
+
+//! Project version string for ACBRadialCollectionViewFramework.
+FOUNDATION_EXPORT const unsigned char ACBRadialCollectionViewFrameworkVersionString[];
+
+
